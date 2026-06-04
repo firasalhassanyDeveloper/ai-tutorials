049b2bb918acabb8f8f69cb8e1bf87ae7e2655d4
firas <falhassany@hotmail.com>
2026-06-04 17:14:50 +0200

    Revert "add tests"
    This reverts commit ea985fbda4da4e9e129d3eeedbefa4b337d85783.

--- spring-ai-model-runner/src/test/java/nl/mandq/springaimodelrunner/SpringAiModelRunnerApplicationTests.java
+++ spring-ai-model-runner/src/test/java/nl/mandq/springaimodelrunner/SpringAiModelRunnerApplicationTests.java
@@ -1,48 +1,13 @@
 package nl.mandq.springaimodelrunner;
 
-import nl.mandq.springaimodelrunner.controller.ChatController;
 import org.junit.jupiter.api.Test;
-import org.springframework.ai.chat.client.ChatClient;
-import org.springframework.ai.model.chat.client.autoconfigure.ChatClientAutoConfiguration;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-@WebMvcTest(
-        controllers = ChatController.class,
-        excludeAutoConfiguration = {
-                ChatClientAutoConfiguration.class
-        }
-)
-class ChatControllerTest {
-
-    @Autowired
-    private MockMvc mockMvc;
-
-    @MockBean
-    private ChatClient chatClient;
+@SpringBootTest
+class SpringAiModelRunnerApplicationTests {
 
     @Test
-    void chat_returnsResponse() throws Exception {
-
-        var requestSpec = mock(ChatClient.ChatClientRequestSpec.class);
-        var responseSpec = mock(ChatClient.CallResponseSpec.class);
-
-        when(chatClient.prompt("Hello AI")).thenReturn(requestSpec);
-        when(requestSpec.call()).thenReturn(responseSpec);
-        when(responseSpec.content()).thenReturn("Hi human");
-
-        mockMvc.perform(post("/chat")
-                        .content("Hello AI")
-                        .contentType("text/plain"))
-                .andExpect(status().isOk())
-                .andExpect(content().string("Hi human"));
+    void contextLoads() {
     }
+
 }
